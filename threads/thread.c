8510c375b3e4d110f1820e7c269529caf50ece02
Zach Saltzman <zach.saltzman@mail.utoronto.ca>
2015-10-17 17:56:09 -0400

    thread_yield() and thread_create() are correct. Have not implemented thread_exit()

--- threads/thread.c
+++ threads/thread.c
@@ -17,7 +17,8 @@ struct thread {
   Tid thread_id;
   int state; //takes a value either READY, RUNNING, or EXITED
   ucontext_t context;
-  int pc_yield;
+  void *stack_malloc_address; //used to keep track of the bottom of the memory we allocated for ths stack
+  int pc_yield; //the number of times we've yielded. used to ensure we aren't stuck in a setcontext loop
   int kernel_thread_first_running; //used to ensure the kernel thread is enqueued properly
 };
 
@@ -130,7 +131,10 @@ void print_queue(thread_queue *tq)
   queue_node *qn = tq->head;
   printf("elements of the queue: \n");
   while(qn != NULL)
-    printf("%i",(int)(qn->node_t->thread_id));
+    {
+    printf("%i\n",(int)(qn->node_t->thread_id));
+    qn = qn->next;
+    }
 }
 
 //GLOBAL DATA STRUCTURES:
@@ -157,15 +161,20 @@ thread_init(void)
    tid_list[i] = 0;
    i++;
   }
-  
+
+
   //set up the thread we're running on right now, i.e. the one supplied by the OS
   struct thread *kernel_thread = (struct thread *)malloc(sizeof(struct thread));
+  
   //since we can't copy the current context into our kernel thread and update it at the same time,
   //we'll need to handle this case explicitly.
   kernel_thread->kernel_thread_first_running = 1;
+  
   //defined to be first thread
   kernel_thread->thread_id = 0;
   kernel_thread->pc_yield = 0;
+  tid_list[0] = 1;
+  
   //since the kernel thread is currently being executed
   kernel_thread->state = RUNNING;
   running_thread = kernel_thread;
@@ -189,14 +198,13 @@ thread_id()
   return running_thread->thread_id;
 }
 
-//TODO: make the stack work. Check the byte allignment
-//ss_sp points to the top (low address of the stack)
-//RSP points to the current location on the stack
+
 Tid
-thread_create(void (*fn) (void *), void *arg)
+thread_create(void (*fn) (void *), void *parg)
 {
-  // printf("called thread_create");
   struct thread *t = (struct thread *)malloc(sizeof(struct thread));
+  if(t == NULL)
+    return THREAD_NOMEMORY;
   //find a thread id we can use and update that spot in the thread list.
   int i = 1; //NOTE: the zeroeth spot will always be taken as long as this program is running.
   while(i<THREAD_MAX_THREADS && tid_list[i] == 1)
@@ -208,97 +216,119 @@ thread_create(void (*fn) (void *), void *arg)
       t->thread_id = i;
       tid_list[i] = 1;
     }
+  
   //copy the current context into the thread's context
   //note that we will be changing our thread's context to reflect the content of the new thread
-  getcontext(&(t->context));
+  getcontext(&t->context);
+  
   //update registers in our context:
   //set instruction pointer (RIP) to the start of thread_stub
   //set first argument register (RDI) to the address of the function we want to pass
   //set second argument register (RSI) to the address of our argument (whatever it is)
-  t->context.uc_mcontext.gregs[REG_RIP] =(long long int) &thread_stub;
-  t->context.uc_mcontext.gregs[REG_RDI] =(long long int) &fn;
-  t->context.uc_mcontext.gregs[REG_RSI] =(long long int) &arg;
 
+  
+  t->context.uc_mcontext.gregs[REG_RIP] =(long long int) &thread_stub;
+  t->context.uc_mcontext.gregs[REG_RDI] =(long long int) fn;
+  t->context.uc_mcontext.gregs[REG_RSI] =(long long int) parg;
+
+  //printf("%#010lx\n",(unsigned long)t->context.uc_mcontext.gregs[REG_RIP]);
+  //printf("%#010lx\n",(unsigned long)&thread_stub);
   t->kernel_thread_first_running = 0;
   t->pc_yield = 0;
   //allocate the stack
   void *stack_pointer;
-  //NOTE: we may have to change this byte allocation
-  stack_pointer = malloc(THREAD_MIN_STACK*BYTE_ALIGNMENT); //Stack is alligned to 16 bytes
-  t->context.uc_stack.ss_sp = stack_pointer; //malloc gives you the top of the stack, which we want ss_sp to be
-  t->context.uc_stack.ss_size = THREAD_MIN_STACK * BYTE_ALIGNMENT;
+  stack_pointer = malloc(THREAD_MIN_STACK); 
   if(stack_pointer == NULL)
     {
       free(t); //if our stack is invalid then this thread should be freed
       return THREAD_NOMEMORY;
     }
-  //this should return a pointer to the TOP of the stack. Note that the stack grows downward (lowest address is the top of the stack in
-  //our particular architecture.) Since malloc will return the lowest address, we have to increment the stack pointer by the size of the stack
-  stack_pointer += BYTE_ALIGNMENT * THREAD_MIN_STACK;
+  
+  //We have have to adjust the first pointer in order to be byte addressable to 16
+  //Note that the stack grows downwards (lowest address is the top of the stack)
+  long int alignment_factor_top = 0;
+  long int alignment_factor_bot = 0;
+  while ((long long int)(stack_pointer+alignment_factor_top) % 16 != 0)
+    alignment_factor_top++;
+  
+  t->context.uc_stack.ss_sp = stack_pointer+alignment_factor_top; //malloc gives you the top of the stack, which we want ss_sp to be;
+  
+  
+  while((long long int)((stack_pointer+THREAD_MIN_STACK-1)-alignment_factor_bot)%16 != 0)
+    alignment_factor_bot++;
 
   //set the stack pointer to be the top of our dynamically allocated stack.
-  t->context.uc_mcontext.gregs[REG_RSP] = (long long int) stack_pointer;
+  t->context.uc_mcontext.gregs[REG_RSP] = (long long int) stack_pointer+THREAD_MIN_STACK-alignment_factor_bot;
+  t->context.uc_stack.ss_size = THREAD_MIN_STACK-alignment_factor_top-alignment_factor_bot;
 
+  //when we free the stack, we'll need the address of the bottom of the memory addresses we allocated.
+  t->stack_malloc_address = stack_pointer;
+  t->kernel_thread_first_running = 0;
+  
   //put the thread in our ready queue
   queue_node *qn = (queue_node *)malloc(sizeof(queue_node));
   qn->node_t = t;
   enqueue(ready_queue, qn);
-  print_queue(ready_queue);
+  printf("thread_id of our thread: %i\n",t->thread_id);
   return t->thread_id;  
 }
 
 Tid
 thread_yield(Tid want_tid)
 {
+  Tid ret; 
   //clean up the threads in the exit queue
 
   //check if we've got a valid thread for the Tid we're given
   //note that we should make sure our tid is positive, otherwise our enum values for threads won't work.
-  if(want_tid >= 0)
-    {
-  if (tid_list[want_tid] == 0)
-    return THREAD_INVALID;
-  if (want_tid == THREAD_ANY && ready_queue->head == NULL) //no threads available to run
-    return THREAD_NONE;
+  if(want_tid >= -2 && want_tid < THREAD_MAX_THREADS )
+    {     
+      if (want_tid == THREAD_ANY && ready_queue->head == NULL) //no threads available to run
+        return THREAD_NONE;
+      if (want_tid >= 0 && tid_list[want_tid] == 0)
+        return THREAD_INVALID;
     }
+  else
+    return THREAD_INVALID;
+  
+  
   //if we get to this point we know we have a valid thread to run.
-  //place the running thread back in the ready queue.
+  //place the running thread back in the ready queue and save its context here.
+  // print_queue(ready_queue);
   queue_node *qn = (queue_node *)malloc(sizeof(queue_node));
   qn->node_t = running_thread;
   enqueue(ready_queue,qn);
-
-  //if any thread is acceptable to run, pop the head of the queue and run it.
-  if(want_tid == THREAD_ANY)
-    running_thread = dequeue_head(ready_queue);
-  if(want_tid == THREAD_SELF)
-    running_thread = dequeue_id(ready_queue,qn->node_t->thread_id);
-  else
-    running_thread = dequeue_id(ready_queue, want_tid);
-
-  printf("Our thread has thread id %i\n",(int)running_thread->thread_id);
-  //run the new thread
-  if(running_thread->kernel_thread_first_running == 1) //we know we need to set up the kernel thread
-    {
-      running_thread->pc_yield = 0;
-      getcontext(&running_thread->context);
-    }
+  getcontext(&qn->node_t->context);
   if(running_thread->pc_yield == 0)
    {
-     running_thread->pc_yield = 1;
+     running_thread->pc_yield = 1; //make sure the thread that is yielding, i.e. the one that is currently
+                                   //running doesn't come in here twice.
+     
+     //if any thread is acceptable to run, pop the head of the queue and run it.
+     if(want_tid == THREAD_ANY)
+       running_thread = dequeue_head(ready_queue);
+     else if(want_tid == THREAD_SELF)
+       running_thread = dequeue_id(ready_queue,qn->node_t->thread_id);
+     else
+       running_thread = dequeue_id(ready_queue, want_tid);
+     ret = running_thread->thread_id;
+     fflush(stdout);
      setcontext(&running_thread->context);
    }
-  printf("We get to return\n");
-  
+
   //we'll eventually come back to this point once our thread runs again, so return the value
   //of the thread that took over.
-  return running_thread->thread_id;
+  //we also reset the flag of the thread that had previously yielded to make sure that we can yield again.
+  
+  running_thread->pc_yield = 0;
+  return ret;
 }
 
 Tid
 thread_exit(Tid tid)
 {
-	TBD();
-	return THREAD_FAILED;
+  //TBD();
+	return 1;
 }
 
 /*******************************************************************
