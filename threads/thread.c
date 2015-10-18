2a435f50653f0ce2cf38a634c17846585963d52f
Zach Saltzman <zach.saltzman@mail.utoronto.ca>
2015-10-18 13:14:41 -0400

    Working code for lab2

--- threads/thread.c
+++ threads/thread.c
@@ -76,11 +76,15 @@ void enqueue(thread_queue *tq, queue_node *qn)
 //note that the thread is not destroyed by the deletion of the queue node.
 struct thread *dequeue_head(thread_queue *tq)
 {
-  struct thread *t_ret;
-  queue_node *q_delete;
-  q_delete = tq->head;
-  t_ret = tq->head->node_t;
-  tq->head = tq->head->next;
+  struct thread *t_ret = NULL;
+  queue_node *q_delete = NULL;
+  if (tq->head != NULL)
+  {
+    q_delete = tq->head;
+    t_ret = tq->head->node_t;
+    tq->head = tq->head->next;
+  }
+  
   delete_node(q_delete);
   return t_ret;
 }
@@ -109,6 +113,11 @@ struct thread *dequeue_id(thread_queue *tq,Tid t_id)
   else if(index == tq->head) //thread is first entry in queue
     {
       t_ret = tq->head->node_t;
+      /*printf("removing node %i and setting head ",tq->head->node_t->thread_id);
+      if(tq->head->next != NULL)
+      printf("to node with id %i\n",tq->head->next->node_t->thread_id);
+      else
+      printf("\n");*/
       tq->head = tq->head->next;
     } 
   else if(index->next == NULL) //thread is at back of list
@@ -132,9 +141,10 @@ void print_queue(thread_queue *tq)
   printf("elements of the queue: \n");
   while(qn != NULL)
     {
-    printf("%i\n",(int)(qn->node_t->thread_id));
+    printf("%i  ",(int)(qn->node_t->thread_id));
     qn = qn->next;
     }
+  printf("\n");
 }
 
 //GLOBAL DATA STRUCTURES:
@@ -185,9 +195,20 @@ thread_init(void)
 void thread_stub(void (*thread_main)(void *), void *arg)
 {
   Tid ret;
-
+  
+  //clear out exit queue, since this thread is guaranteed to not be exiting.
+  //note that dequeue_head implicitly frees the node it pops.
+  struct thread *index_thread = dequeue_head(exit_queue);
+  while(index_thread != NULL)
+  {
+    free(index_thread->stack_malloc_address); //the original, pre-alignment address given to us by malloc
+    free(index_thread);
+    index_thread = dequeue_head(exit_queue);
+  }
+  
   thread_main(arg);
   ret = thread_exit(THREAD_SELF);
+  //print_queue(ready_queue);
   assert(ret == THREAD_NONE);
   exit(0);
 }
@@ -198,21 +219,28 @@ thread_id()
   return running_thread->thread_id;
 }
 
-
+//NOTE: problem is coming from queue not being set properly (skipping ahead nodes for some reason)
 Tid
 thread_create(void (*fn) (void *), void *parg)
 {
   struct thread *t = (struct thread *)malloc(sizeof(struct thread));
   if(t == NULL)
+    {
+      printf("our threads have no memory\n");
     return THREAD_NOMEMORY;
+    }
   //find a thread id we can use and update that spot in the thread list.
   int i = 1; //NOTE: the zeroeth spot will always be taken as long as this program is running.
   while(i<THREAD_MAX_THREADS && tid_list[i] == 1)
     i++;
-  if(i+1 == THREAD_MAX_THREADS) //we are at capacity for threads.
-    return THREAD_NOMORE;
+  if(i == THREAD_MAX_THREADS) //we are at capacity for threads.
+    {
+      // printf("we are at capacity for threads\n");
+      return THREAD_NOMORE;
+    }
   else
     {
+      // printf("setting tid_list[%d] to 1\n",i);
       t->thread_id = i;
       tid_list[i] = 1;
     }
@@ -231,8 +259,6 @@ thread_create(void (*fn) (void *), void *parg)
   t->context.uc_mcontext.gregs[REG_RDI] =(long long int) fn;
   t->context.uc_mcontext.gregs[REG_RSI] =(long long int) parg;
 
-  //printf("%#010lx\n",(unsigned long)t->context.uc_mcontext.gregs[REG_RIP]);
-  //printf("%#010lx\n",(unsigned long)&thread_stub);
   t->kernel_thread_first_running = 0;
   t->pc_yield = 0;
   //allocate the stack
@@ -269,7 +295,6 @@ thread_create(void (*fn) (void *), void *parg)
   queue_node *qn = (queue_node *)malloc(sizeof(queue_node));
   qn->node_t = t;
   enqueue(ready_queue, qn);
-  printf("thread_id of our thread: %i\n",t->thread_id);
   return t->thread_id;  
 }
 
@@ -277,13 +302,11 @@ Tid
 thread_yield(Tid want_tid)
 {
   Tid ret; 
-  //clean up the threads in the exit queue
-
   //check if we've got a valid thread for the Tid we're given
   //note that we should make sure our tid is positive, otherwise our enum values for threads won't work.
   if(want_tid >= -2 && want_tid < THREAD_MAX_THREADS )
     {     
-      if (want_tid == THREAD_ANY && ready_queue->head == NULL) //no threads available to run
+      if (want_tid == THREAD_ANY && ready_queue->head== NULL) //no threads available to run
         return THREAD_NONE;
       if (want_tid >= 0 && tid_list[want_tid] == 0)
         return THREAD_INVALID;
@@ -294,7 +317,6 @@ thread_yield(Tid want_tid)
   
   //if we get to this point we know we have a valid thread to run.
   //place the running thread back in the ready queue and save its context here.
-  // print_queue(ready_queue);
   queue_node *qn = (queue_node *)malloc(sizeof(queue_node));
   qn->node_t = running_thread;
   enqueue(ready_queue,qn);
@@ -303,7 +325,6 @@ thread_yield(Tid want_tid)
    {
      running_thread->pc_yield = 1; //make sure the thread that is yielding, i.e. the one that is currently
                                    //running doesn't come in here twice.
-     
      //if any thread is acceptable to run, pop the head of the queue and run it.
      if(want_tid == THREAD_ANY)
        running_thread = dequeue_head(ready_queue);
@@ -311,24 +332,88 @@ thread_yield(Tid want_tid)
        running_thread = dequeue_id(ready_queue,qn->node_t->thread_id);
      else
        running_thread = dequeue_id(ready_queue, want_tid);
+   
      ret = running_thread->thread_id;
-     fflush(stdout);
      setcontext(&running_thread->context);
    }
+
+  //we also clear the exit queue here to ensure that exited threads are cleaned up whenever another thread runs
+  struct thread *index_thread = dequeue_head(exit_queue);
+  while(index_thread != NULL)
+  {
+    free(index_thread->stack_malloc_address); //the original, pre-alignment address given to us by malloc
+    free(index_thread);
+    index_thread = dequeue_head(exit_queue);
+  }
 
   //we'll eventually come back to this point once our thread runs again, so return the value
   //of the thread that took over.
   //we also reset the flag of the thread that had previously yielded to make sure that we can yield again.
-  
   running_thread->pc_yield = 0;
   return ret;
+}
+
+
+//Note that this function is the same as thread_yield, except the yielding thread does not put itself in the ready queue.
+//Used to ensure that thread_exit(THREAD_SELF) works properly. 
+Tid
+thread_yield_from_exit(Tid want_tid)
+{
+  queue_node *qn = (queue_node *)malloc(sizeof(queue_node));
+  qn->node_t = running_thread;
+  tid_list[qn->node_t->thread_id] = 0;
+  enqueue(exit_queue,qn);
+  
+  if(running_thread->pc_yield == 0)
+   {
+     running_thread->pc_yield = 1; //make sure the thread that is yielding, i.e. the one that is currently
+                                   //running doesn't come in here twice.
+    
+     running_thread = dequeue_head(ready_queue);
+     
+     setcontext(&running_thread->context);
+   }
+
+  //This thread should be getting exited, so we should never come back here.
+  running_thread->pc_yield = 0;
+  return THREAD_FAILED;
 }
 
 Tid
 thread_exit(Tid tid)
 {
-  //TBD();
-	return 1;
+  //if we don't have any more threads to run
+  if((tid == THREAD_ANY || tid == THREAD_SELF) && ready_queue->head == NULL)
+    return THREAD_NONE;
+   
+
+  //if the thread isn't in our tid_list
+  if(tid < -2 || tid >= THREAD_MAX_THREADS || (tid > 0 && tid_list[tid] == 0))
+    {
+      
+      return THREAD_INVALID;
+    }
+
+  //if we reach this point we know we have a valid thread
+  queue_node *qn = (queue_node *)malloc(sizeof(queue_node));
+  
+
+  if(tid == THREAD_ANY)
+    {
+      qn->node_t = dequeue_head(ready_queue);
+      tid_list[qn->node_t->thread_id] = 0;
+    }
+  else if(tid == THREAD_SELF) //in this case we must yield to another thread
+    {
+      thread_yield_from_exit(THREAD_ANY); //yield to the next thread in the list.
+    }
+  else
+    {
+      qn->node_t = dequeue_id(ready_queue,tid);
+      tid_list[tid] = 0;
+    }
+    enqueue(exit_queue,qn); //takes a thread out of the ready queue and puts it into the exit queue
+    return qn->node_t->thread_id;
 }
 
 /*******************************************************************
